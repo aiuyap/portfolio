0a3ced29c4caee6ece2650b4b1a087f2774aee16
aiuyap <186591613+aiuyap@users.noreply.github.com>
2025-01-23 16:57:10 +0800

    feat: added some sample content for learning journey section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
 function App() {
   return (
     <>
-      <nav className="sticky flex h-10 items-center justify-center gap-16">
+      <nav className="sticky top-0 flex h-10 items-center justify-center gap-16">
         <a href="#">Home</a>
         <a href="#">Learning Journey</a>
         <a href="#">Projects</a>
       </nav>
       <section className="flex h-dvh items-center justify-center">
         <h1>Hi, I&apos;m Aiu. An aspiring Software Engineer</h1>
+      </section>
+      <section className="flex h-dvh flex-col items-center justify-center">
+        <h1>Learning Journey</h1>
+        <p>
+          I am a graduate of Bachelor of Science in Information Technology at
+          University of Cebu - Banilad Campus on 2019. 3 months ago, I started
+          The Odin Project to learn about Web Development. I managed to finish
+          the Foundation Course and currently on track with the Full Stack
+          JavaScript course. This course taught me a lot of core fundamentals of
+          web development which I believe will definitely kickstart my career as
+          a Software Engineer.
+        </p>
+        <h2>Tech Stack</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>SQL</li>
+          <li>NodeJS</li>
+          <li>npm</li>
+          <li>Git</li>
+          <li>GitHub</li>
+          <li>Jest</li>
+          <li>Webpack</li>
+          <li>TailwindCSS</li>
+          <li>Vite</li>
+          <li>shadcn/ui</li>
+          <li>Lucide</li>
+          <li>date fns</li>
+        </ul>
       </section>
     </>
   );
