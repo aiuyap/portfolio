58602a766865a6d805967cd59536b8a769c4f9e6
aiuyap <186591613+aiuyap@users.noreply.github.com>
2025-01-23 12:37:34 +0800

    feat: installed tailwind and auto class sorting

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <nav className="sticky flex h-10 items-center justify-center gap-16">
+        <a href="#">Home</a>
+        <a href="#">Learning Journey</a>
+        <a href="#">Projects</a>
+      </nav>
+      <section className="flex h-dvh items-center justify-center">
+        <h1>Hi, I&apos;m Aiu. An aspiring Software Engineer</h1>
+      </section>
+    </>
+  );
 }
 
 export default App;
