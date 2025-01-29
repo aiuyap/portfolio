98661b11ddc6f59c7dcadd5aa106f5ad5541be59
aiuyap <186591613+aiuyap@users.noreply.github.com>
2025-01-29 09:57:41 +0800

    refactor: Separated each section to their own module

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { LearningJourney } from "./LearningJourney";
+import { Projects } from "./Project";
+
 function App() {
   return (
     <>
@@ -7,39 +10,12 @@ function App() {
         <a href="#">Projects</a>
       </nav>
       <section className="flex h-dvh items-center justify-center">
-        <h1>Hi, I&apos;m Aiu. An aspiring Software Engineer</h1>
+        <h1 className="text-2xl">
+          Hi, I&apos;m Aiu. An aspiring Software Engineer
+        </h1>
       </section>
-      <section className="flex h-dvh flex-col items-center justify-center">
-        <h1>Learning Journey</h1>
-        <p>
-          I am a graduate of Bachelor of Science in Information Technology at
-          University of Cebu - Banilad Campus on 2019. 3 months ago, I started
-          The Odin Project to learn about Web Development. I managed to finish
-          the Foundation Course and currently on track with the Full Stack
-          JavaScript course. This course taught me a lot of core fundamentals of
-          web development which I believe will definitely kickstart my career as
-          a Software Engineer.
-        </p>
-        <h2>Tech Stack</h2>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>SQL</li>
-          <li>NodeJS</li>
-          <li>npm</li>
-          <li>Git</li>
-          <li>GitHub</li>
-          <li>Jest</li>
-          <li>Webpack</li>
-          <li>TailwindCSS</li>
-          <li>Vite</li>
-          <li>shadcn/ui</li>
-          <li>Lucide</li>
-          <li>date fns</li>
-        </ul>
-      </section>
+      <LearningJourney />
+      <Projects />
     </>
   );
 }
---
+++ src/LearningJourney.jsx
@@ -0,0 +1,35 @@
+export function LearningJourney() {
+  return (
+    <section className="flex h-dvh flex-col items-center justify-center px-14">
+      <h1 className="pb-14 text-2xl">Learning Journey</h1>
+      <p>
+        I am a graduate of Bachelor of Science in Information Technology at
+        University of Cebu - Banilad Campus on 2019. 3 months ago, I started The
+        Odin Project to learn about Web Development. I managed to finish the
+        Foundation Course and currently on track with the Full Stack JavaScript
+        course. This course taught me a lot of core fundamentals of web
+        development which I believe will definitely kickstart my career as a
+        Software Engineer.
+      </p>
+      <h2 className="pt-14 text-2xl">Tech Stack</h2>
+      <ul className="flex items-center justify-center gap-14 pt-14">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>SQL</li>
+        <li>NodeJS</li>
+        <li>npm</li>
+        <li>Git</li>
+        <li>GitHub</li>
+        <li>Jest</li>
+        <li>Webpack</li>
+        <li>TailwindCSS</li>
+        <li>Vite</li>
+        <li>shadcn/ui</li>
+        <li>Lucide</li>
+        <li>date fns</li>
+      </ul>
+    </section>
+  );
+}
